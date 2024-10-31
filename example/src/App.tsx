c6f4207c272e80b81b475f7be6e5d1fc48334395
吴敬悦 <wutiange20@qq.com>
2024-10-31 15:58:25 +0800

    feat: 修改界面

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import {
   StyleSheet,
   View,
-  Text,
   PermissionsAndroid,
   type Permission,
 } from 'react-native';
 import { fetch, type NetInfoWifiState } from '@react-native-community/netinfo';
+import { TextInput, Button, Text } from 'react-native-paper';
 
 const checkAndRequestPermission = async () => {
   try {
@@ -28,14 +28,35 @@ const checkAndRequestPermission = async () => {
   }
 };
 
+const is5G = (frequency: number) => {
+  return frequency > 4900 && frequency < 5900;
+};
+
 export default function App() {
   const [wifiInfo, setWifiInfo] = useState<NetInfoWifiState>();
+
+  const [apPassword, setApPassword] = useState('');
+  const [deviceCount, setDeviceCount] = useState('');
+  const [aesKey, setAesKey] = useState('');
+  const [customData, setCustomData] = useState('');
+  const [hint, setHint] = useState('');
+  const [message, setMessage] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [aesKeyVisible, setAesKeyVisible] = useState(false);
   useEffect(() => {
-    checkAndRequestPermission().finally(() => {
-      fetch().then((state) => {
-        setWifiInfo(state as NetInfoWifiState);
+    checkAndRequestPermission()
+      .catch(() => {
+        setMessage('需要位置权限来获取 Wi-Fi 信息。 \n点击申请权限');
+      })
+      .finally(() => {
+        fetch().then((state) => {
+          const tempState = state as NetInfoWifiState;
+          setWifiInfo(tempState);
+          if (is5G(tempState?.details?.frequency ?? 0)) {
+            setHint('当前连接的是 5G Wi-Fi, 请确定您的设备是否支持。');
+          }
+        });
       });
-    });
   }, []);
 
   const wifiArr = [
@@ -43,6 +64,10 @@ export default function App() {
     { label: 'BSSID', value: wifiInfo?.details?.bssid },
     { label: 'IP', value: wifiInfo?.details?.ipAddress },
   ];
+
+  const handleConfirm = () => {
+    // 处理确认按钮点击事件
+  };
 
   return (
     <View style={styles.container}>
@@ -52,6 +77,62 @@ export default function App() {
           <Text>{item.value}</Text>
         </View>
       ))}
+      <TextInput
+        mode="outlined"
+        label="WiFi密码"
+        value={apPassword}
+        onChangeText={setApPassword}
+        secureTextEntry={!passwordVisible}
+        right={
+          <TextInput.Icon
+            icon={passwordVisible ? 'eye-off' : 'eye'}
+            onPress={() => setPasswordVisible(!passwordVisible)}
+          />
+        }
+        style={styles.input}
+      />
+
+      <TextInput
+        mode="outlined"
+        label="设备数量"
+        value={deviceCount}
+        onChangeText={setDeviceCount}
+        keyboardType="numeric"
+        style={styles.input}
+      />
+
+      <TextInput
+        mode="outlined"
+        label="AES密钥"
+        value={aesKey}
+        onChangeText={setAesKey}
+        secureTextEntry={!aesKeyVisible}
+        right={
+          <TextInput.Icon
+            icon={aesKeyVisible ? 'eye-off' : 'eye'}
+            onPress={() => setAesKeyVisible(!aesKeyVisible)}
+          />
+        }
+        style={styles.input}
+      />
+
+      <TextInput
+        mode="outlined"
+        label="自定义数据"
+        value={customData}
+        onChangeText={setCustomData}
+        style={styles.input}
+      />
+
+      {hint ? <Text style={styles.hintText}>{hint}</Text> : null}
+
+      <View style={styles.messageContainer}>
+        <Text>{message}</Text>
+      </View>
+
+      <Button mode="contained" onPress={handleConfirm} style={styles.button}>
+        确认
+      </Button>
     </View>
   );
 }
@@ -69,5 +150,19 @@ const styles = StyleSheet.create({
   label: {
     marginRight: 8,
     color: '#ff5983',
+  },
+  input: {},
+  hintText: {
+    color: 'red',
+  },
+  messageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 16,
+  },
+  button: {
+    marginTop: 16,
+    marginBottom: 16,
   },
 });
